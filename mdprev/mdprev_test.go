5e55e0cf17153cce5fa18b6086c8a9ad1ff61100
Michal Wrobel <sparrovv@gmail.com>
2014-12-08 20:17:40 +0000

    Rename local var

--- mdprev/mdprev_test.go
+++ mdprev/mdprev_test.go
@@ -21,31 +21,31 @@ func TestNewMdPrev(t *testing.T) {
 		panic(err)
 	}
 
-	mdp := NewMdPrev(mdFile.Name())
-	if mdp.MdContent != expectedContent {
-		t.Fatalf("The file's contents: %s is not eql to the expected %s", mdp.MdContent, expectedContent)
+	mdPrev := NewMdPrev(mdFile.Name())
+	if mdPrev.MdContent != expectedContent {
+		t.Fatalf("The file's contents: %s is not eql to the expected %s", mdPrev.MdContent, expectedContent)
 	}
 
-	if len(mdp.WSConns) != 0 {
-		t.Fatalf("Expecting no WS connections, but got %s", len(mdp.WSConns))
+	if len(mdPrev.WSConns) != 0 {
+		t.Fatalf("Expecting no WS connections, but got %s", len(mdPrev.WSConns))
 	}
 }
 
 func TestWatcher(t *testing.T) {
 	expectedContent := "Content"
-	mdp := testMdprevObj("")
-	defer os.Remove(mdp.MdFile)
+	mdPrev := testMdprevObj("")
+	defer os.Remove(mdPrev.MdFile)
 
-	mdp.Watch()
+	mdPrev.Watch()
 
 	var expectedChange bool
 	expectedChange = false
 
 	go func() {
-		expectedChange = <-mdp.MdChanges
+		expectedChange = <-mdPrev.MdChanges
 	}()
 
-	_ = ioutil.WriteFile(mdp.MdFile, []byte(expectedContent), 0644)
+	_ = ioutil.WriteFile(mdPrev.MdFile, []byte(expectedContent), 0644)
 	// it sucks, but watcher needs time to notice changes
 	time.Sleep(10 * time.Millisecond)
 
@@ -56,13 +56,13 @@ func TestWatcher(t *testing.T) {
 
 func TestUpdateWSConnections(t *testing.T) {
 	content := "A lot of new content"
-	mdp := testMdprevObj(content)
-	defer os.Remove(mdp.MdFile)
+	mdPrev := testMdprevObj(content)
+	defer os.Remove(mdPrev.MdFile)
 	var b *bytes.Buffer = new(bytes.Buffer)
 
-	mdp.KeepWSConn(b)
-	mdp.UpdateWSConnections()
-	mdp.MdChanges <- true
+	mdPrev.KeepWSConn(b)
+	mdPrev.UpdateWSConnections()
+	mdPrev.MdChanges <- true
 
 	if b.String() != content {
 		t.Errorf("Expecting that io.Writer gets %s, but got %s", "foo", b.String())
